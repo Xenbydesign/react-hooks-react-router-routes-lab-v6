b76efb8a88b2027fe5fc62003d0fabbb19a416ae
Xennia Contreras <Xenbydesign@gmail.com>
2024-03-01 09:22:37 -0800

    completed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,14 @@
 import {Link} from 'react-router-dom';
 
-function MovieCard({title}) {
+//from the movies passed i can pull just what i need as a pram like the title or id 
+function MovieCard({title, id}) {
   
   return (
     <article>
         <h2>{title}</h2>
-        {/* What should go here? */}
+        <Link to={`/movie/${id}`}>
+        View Info
+      </Link>
     </article>
   );
 };
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,42 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const params = useParams();
+  const movieId = params.id;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+      .then(response => response.json())
+      .then(data => setMovie(data))
+      .catch(error => console.error(error));
+  }, [movieId]);
+
+  if (!movie.id) {
+    return <h1>Loading Movie.</h1>;
+  }
+
+
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>Time: {movie.time}</p>
+        <div>
+          <span>Genres: </span>
+          {movie.genres && movie.genres.map((genre, index) => (
+            <span key={index}>{genre}</span>
+          ))}
+        </div>
       </main>
     </>
   );
-};
+}
 
 export default Movie;
